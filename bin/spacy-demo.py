7a87572287ff12c5285996a16753a4ded88e2a8d
Clifford Wulfman <cwulfman@princeton.edu>
2019-10-03 12:12:13 -0400

    Adds brief spacy demo.

--- bin/spacy-demo.py
+++ bin/spacy-demo.py
@@ -1,4 +1,28 @@
+""" Brief spacy demo.
+
+Runs standard spacy processor on text from
+stdin and displays it with spacy.
+
+Usage python3 spacy-demo.py < path/to/input
+
+"""
+
+import sys
 import spacy
+from spacy import displacy
 
 
+# load a model
 nlp = spacy.load("en_core_web_sm")
+
+# load a file to process
+# with open('path/to/file', 'r') as reader:
+#     text = reader.read()
+
+text = sys.stdin.read()
+
+# process the text
+doc = nlp(text)
+
+# view the entities
+displacy.serve(doc, style="ent")
